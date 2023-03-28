bb11670eda825ee067391279da3139fdd5245ea8
nafeel <nknappy123@gmail.com>
2023-03-28 07:48:30 +0530

    feat: wip configured react-navigation

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {MovieListView} from '../screens';
+
+const Stack = createStackNavigator();
+
+const screenNames = {
+  movieListView: 'MovieList',
+};
+
+const AppNavigator = (): JSX.Element => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={screenNames.movieListView}
+          component={MovieListView}
+          options={{
+            title: 'Top Rated Movies',
+            headerStyle: {
+              backgroundColor: '#002138',
+            },
+            headerTintColor: 'white',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ src/screens/index.tsx
@@ -0,0 +1,1 @@
+export {default as MovieListView} from './movieList/MovieListView';
